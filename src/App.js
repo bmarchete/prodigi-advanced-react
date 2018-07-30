5130e46b6a1ceb47dfd82d014d97e4d73cc82139
bmarchete <bmarchete@gmail.com>
2018-07-30 01:48:14 -0400

    Adds and tests materialize components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import TestFechtAPI from "./components/TestFechtAPI";
-
+import FloatingActionButtons from "./components/FloatingActionButtons";
 class App extends Component {
   render() {
     return (
       <div>
         <TestFechtAPI />
+        <FloatingActionButtons />
       </div>
     );
   }
