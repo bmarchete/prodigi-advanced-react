6a4f28618e486aa25d1c113aecee74f58f8555b1
bmarchete <bmarchete@gmail.com>
2018-08-09 16:56:39 -0400

    Fixes Home component exclusion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-import Home from "./components/Home";
 import ListCompanies from "./containers/ListCompanies";
 import FloatingActionButtons from "./components/FloatingActionButtons";
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <Home /> */}
         <ListCompanies />
         {/* <FloatingActionButtons /> */}
       </div>
