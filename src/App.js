052aad56c4fcf9c7aabba40fea285f428773c298
bmarchete <bmarchete@gmail.com>
2018-08-17 00:59:43 -0400

    Adds Router components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import ListCompanies from "./containers/ListCompanies";
-import FloatingActionButtons from "./components/FloatingActionButtons";
+import MenuAppBar from "./containers/NavBar/MenuAppBar";
+import { Switch, Route } from "react-router-dom";
+import RouteTest from "./components/RouteTest";
 class App extends Component {
   render() {
     return (
+      // <RouteTest />
       <div>
-        <ListCompanies />
-        {/* <FloatingActionButtons /> */}
+        <MenuAppBar />
+        <Switch>
+          <Route exact path="/" component={RouteTest} />
+          <Route exact path="/list-companies" component={ListCompanies} />
+          {/* <FloatingActionButtons /> */}
+        </Switch>
       </div>
     );
   }
